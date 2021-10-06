382e5796447fdad6176cffd2797f2a5fe063f9a5
MK954481 <mayankkumar_co20b5_04@dtu.ac.in>
2021-10-06 15:45:46 +0530

    home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import PathFindingVisualizer from "./components/PathFindingVisualizer/PathFindingVisualizer";
 import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer";
-import Lecture from "./Pages/Home2/Home";
+import HomePage from "./Pages/Home/Home";
 import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Lecture />
+      <HomePage />
       {/* <SortingVisualizer /> */}
       {/* <PathFindingVisualizer /> */}
     </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,68 @@
 import React from "react";
 import "./Home.css";
+import Carousel from "react-elastic-carousel";
 
 const HomePage = () => {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 1 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 3 },
+  ];
   return (
-    <div>
-      <div className="homePage">
-        <div className="homeSection"></div>
+    <div className="homePage">
+      <div className="searchsort">
+        <span className="head">Searching &amp; Sorting</span>
+        <hr />
+        <Carousel breakPoints={breakPoints}>
+          <div className="card">
+            <span>Linear Search</span>
+          </div>
+          <div className="card">
+            <span>Binary Search</span>
+          </div>
+          <div className="card">
+            <span>Merge Sort</span>
+          </div>
+          <div className="card">
+            <span>Quick Sort</span>
+          </div>
+          <div className="card">
+            <span>Heap Sort</span>
+          </div>
+        </Carousel>
+      </div>
+      <div className="pathfinding">
+        <span className="head">Pathfinding Algorithms</span>
+        <hr />
+        <Carousel breakPoints={breakPoints}>
+          <div className="card">
+            <span>A*</span>
+          </div>
+          <div className="card">
+            <span>Djkistra</span>
+          </div>
+          <div className="card">
+            <span>Algo3</span>
+          </div>
+          <div className="card">
+            <span>Algo4</span>
+          </div>
+          <div className="card">
+            <span>Algo5</span>
+          </div>
+        </Carousel>
+      </div>
+      <div className="trees">
+        <span className="head">Trees</span>
+        <hr />
+        <Carousel breakPoints={breakPoints}>
+          <div className="card">Binary Tree</div>
+          <div className="card">Binary Search Tree</div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </Carousel>
       </div>
     </div>
   );
